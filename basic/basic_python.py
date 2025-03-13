f054f122afe5e424966514928943572750c120dc
AnuragPatkar <anuragpatkar786@gmail.com>
2025-03-13 15:43:23 +0530

    add

--- basic/basic_python.py
+++ basic/basic_python.py
@@ -10,7 +10,7 @@ print("Sum is ",c)  """
 """
 multi line comment
 """ 
-#type
+""" #type
 x=5
 print(type(x)) #type predefined function h ,jo datatype btata h
 x=5.6
@@ -20,7 +20,7 @@ print(type(x))
 x="India"
 print(type(x))
 x=3+4j
-print(type(x))
+print(type(x)) """
 #char & double is not in python
 #int, float, complex, bool, str is class in python
 #address is called id in python
@@ -34,9 +34,49 @@ print(type(x))
 #python m dynamically object create hota h
 #object= object is something which can store data and has some methods to handle data
 
-import myfile #importing myfile
+""" import myfile #importing myfile
 print(myfile.x) # myfile ke x ko print krne ke liye
 
 import keyword
 print("There are ",len(keyword.kwlist),"keywords in python")
-print(keyword.kwlist)
+print(keyword.kwlist) """
+
+#opetaors
+#1. Arithmetic operators (+,-,*,/,%,//,**)
+#2. Assignment operators (=,+=,-=,*=,/=,%=,//=,**=)
+#3. Comparison operators (==,!=,<,>,<=,>=)
+#4. Logical operators (and,or,not)
+#5. Bitwise operators (&,|,^,~,<<,>>)
+#6. Identity operators (is,is not) #is is used to check the address of two variables
+#7. Membership operators (in,not in) #in is used to check the value in the list or not  #not in is used to check the value is not in the list
+#8. Ternary operators (a if condition else b) #if condition is true then a otherwise b  
+#no ++ or -- operator in python
+
+""" #write a program to check whether a given number is positive or non positive
+x=int(input("Enter a number"))
+if x>0:
+    print("Positive")
+if x<=0:
+    print("Non Positive")
+print("Thank you") """
+
+""" #write a program to check whether a given number is even or odd
+x=int(input("Enter a number"))
+if x%2==0:
+    print("Even")
+else:
+    print("Odd") """
+
+""" #if elif else
+#write a program to check whether a given number is positive or negative or zero"
+x=int(input("Enter a number"))
+if x>0:
+    print("Positive")
+elif x<0:
+    print("Negative")
+else:
+    print("Zero") """
+
+#single line if else
+
+print("Even" if int(input("Enter a number"))%2==0 else "Odd") 
